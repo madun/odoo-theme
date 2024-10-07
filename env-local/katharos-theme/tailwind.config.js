552f7575745419da5c5adf788fc1e5d87d04762d
Ilman Qori <ilmanqori@Ilmans-iMac.local>
2024-10-07 22:21:29 +0700

    setup tailwindcss

---
+++ env-local/katharos-theme/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.xml',
+    './static/src/js/**/*.js',
+    './static/src/css/**/*.scss',
+  ],
+  darkMode: 'class', // atau 'media' atau 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
